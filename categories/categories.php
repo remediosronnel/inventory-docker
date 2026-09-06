16f36db130c9a42f5ebb81ac99a026890820a9fe
remediosronnel <remediosronnel30@gmail.com>
2026-09-07 01:17:54 +0800

    Updated Fixes

--- categories/categories.php
+++ categories/categories.php
@@ -1,10 +1,13 @@
 <?php
-include "../includes/header.php";
-include "../includes/sidebar.php";
 include "../config/database.php";
 
 $result = mysqli_query($conn, "SELECT * FROM categories ORDER BY id DESC");
 
+
+
+
+include "../includes/header.php";
+include "../includes/sidebar.php";
 ?>
 
 <div class="content">
