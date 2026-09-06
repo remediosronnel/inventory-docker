633d6a5629c86c9ef773199b44cf10514bc59e8c
remediosronnel <remediosronnel30@gmail.com>
2026-09-06 21:17:12 +0800

    Update Fixes

--- products/delete_product.php
+++ products/delete_product.php
@@ -1,13 +1,15 @@
-<?php  include "../config/database.php";
+<?php
+include "../config/database.php";
 
-if(isset($_GET['id'])){
-    $id=$_GET['id'];
-    $query = " DELETE FROM products WHERE id=$id  ";
-    mysqli_query($conn,$query);  }
+$id = $_GET['id'];
+
+// Delete related transactions first
+mysqli_query($conn, "DELETE FROM transactions WHERE product_id=$id");
+
+// Delete product
+mysqli_query($conn, "DELETE FROM products WHERE id=$id");
 
 header("Location: ../products.php");
-
 exit();
-
 
 ?>
