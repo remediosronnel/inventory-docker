36e00c4e419d206c9681a7ce74b1e2efd91dc710
remediosronnel <remediosronnel30@gmail.com>
2026-09-06 21:31:39 +0800

    Update Fixes

--- suppliers/delete_supplier.php
+++ suppliers/delete_supplier.php
@@ -1,7 +1,15 @@
 <?php
-
 include "../config/database.php";
-    $id=$_GET['id'];
-    mysqli_query($conn, "DELETE FROM suppliers WHERE id=$id" );
-header("Location:../suppliers.php");
+
+$id = $_GET['id'];
+
+// Remove products connected to this supplier
+mysqli_query($conn, "DELETE FROM products WHERE supplier_id=$id");
+
+// Remove supplier
+mysqli_query($conn, "DELETE FROM suppliers WHERE id=$id");
+
+header("Location: ../suppliers.php");
+exit();
+
 ?>
